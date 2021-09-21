159e3c2389e523c47e030ab308619ef5de51e6c7
Iván Renison <ivan.renison@mi.unc.edu.ar>
2021-09-21 09:12:17 -0300

    Testing

--- xv6-public/flappy.c
+++ xv6-public/flappy.c
@@ -8,17 +8,27 @@
 
 int main(void)
 {
-//  game_status game = malloc(sizeof(struct _s_game_status));
+  VGA_mode_switch(VGA_mode_graphic);
+
+  game_status* game = malloc(sizeof(game_status));
+
+  game->hole_tubes_y[0] = 100;
+  game->hole_tubes_y[1] = 100;
+  game->hole_tubes_y[2] = 100;
+  game->hole_tubes_y[3] = 100;
+  game->hole_tubes_y[4] = 100;
+
+  game->first_tube_x = 20;
 
   uchar* buffer = malloc(VGA_graphic_hight*VGA_graphic_width);
 
-//  draw_pixel(buffer, 0, 0, 0);
   draw_sky(buffer);
   draw_clouds(buffer);
   draw_bush(buffer);
   draw_ground(buffer);
 
-  VGA_mode_switch(VGA_mode_graphic);
+  draw_tubes(game, buffer);
+
 
   VGA_plot_screen(buffer);
 
