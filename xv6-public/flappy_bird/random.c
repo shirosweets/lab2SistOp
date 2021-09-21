1b8b216fa486a14c2fd32d7347cb8f96665e3be9
Iván Renison <ivan.renison@mi.unc.edu.ar>
2021-09-21 14:34:10 -0300

    Módulo random

---
+++ xv6-public/flappy_bird/random.c
@@ -0,0 +1,52 @@
+#include "../types.h"
+
+#include "random.h"
+
+#define min(a, b) (a < b ? a : b)
+#define abs(x) (0 < x ? x : (-x))
+
+// Algoritmo usado: https://en.wikipedia.org/wiki/Linear-feedback_shift_register
+int
+next_random(int x)
+{
+  // Casteo a uint, por qud necesito que el >> agregue siempre 0 a la izquierda
+  uint a = (uint)(x == 0 ? 1 : x);
+  a = (a >> 1) | (((a >> 0) ^ (a >> 2) ^ (a >> 3) ^ (a >> 5)) << 15);
+
+  return((int)a);
+}
+
+int
+new_random(void)
+{
+  seed = next_random(seed);
+  return(seed);
+}
+
+/* Obtiene el menor x positivo tal que 2^x >= abs(n)
+ */
+static int
+next_power_of_2(int n)
+{
+  n = abs(n);
+  int res = 0;
+  while((1 << res) < n){ // 1 << res es lo mismo que 2^res (<< es un LSL)
+    res++;
+  }
+  return(res);
+}
+
+int
+new_random_less_than(int n)
+{
+  int shift = 8*sizeof(int) - next_power_of_2(n) - 1;
+  int res;
+  do{
+    /* Se crea un nuevo número aletaorio positivo, y se le hace un shift para
+       que quede entre 0 y next_power_of_2(n). Se hace eso las veces haga falta
+       para que quede entre 0 y n */
+    res = new_random();
+    res = res >> shift;
+  } while(abs(res) > abs(n));
+  return(res);
+}
---
+++ xv6-public/flappy_bird/random.h
@@ -0,0 +1,27 @@
+#ifndef RANDOM_H
+#define RANDOM_H
+
+/* Semilla por defecto, se va modificando cada ves que se genera un número aleatorio
+ * Modificar para reseetar semilla
+ */
+int seed = 0;
+
+/* Obtiene un número aleatorio a partir de x
+ * Pasarle el mismo x produse el mismo resultado, pero un pequeño cambio en x
+ * cambia completamente el resultado
+ */
+int next_random(int x);
+
+/* Obtiene un nuevo número aleatorio a partir de la semilla y modifica la semilla
+ */
+int new_random(void);
+
+/* Obtiene en número aleatorio entre -n y n (incluidos)
+ * Lo hace con la semilla, y la modifica
+ *
+ * POS: -abs(n) <= res && res <= abs(n)
+ */
+int new_random_less_than(int n);
+
+#include "random.c"
+#endif // RANDOM_H
