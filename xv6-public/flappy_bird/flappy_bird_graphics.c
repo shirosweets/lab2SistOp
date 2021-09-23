de4b47110074f677a746f2d69a27876e0aa90235
tfuentes <tiffany.fuentes@mi.unc.edu.ar>
2021-09-23 20:26:37 -0300

    Cambios graficos en flappy

--- xv6-public/flappy_bird/flappy_bird_graphics.c
+++ xv6-public/flappy_bird/flappy_bird_graphics.c
@@ -10,10 +10,15 @@
 #define bush_color 0x32
 #define ground_color 0x06
 #define tubes_color 0x02
+#define tubes1_color 0xbe
+#define tubes2_color 0x90
+#define tubes3_color 0x02
+#define line_color 0x0F
 #define flappy_eye_color 0x0F
 #define flappy_pupil_color 0x00
 #define flappy_wing_color 0x2C
 #define flappy_mouth_color 0x28
+#define tubes_shadow_color 0x00
 #define flappy_color 0x00
 
 /* Dibuja un tubo con el centro del hueco en (x, y)
@@ -27,11 +32,30 @@ draw_tube(uchar* buffer, x_coord x, y_coord y)
   y_coord top_part_hole = y - width_hole_tube/2;
 
   // Tubo de arriba
-  draw_rectangle(buffer, left_side, right_side, 0, top_part_hole, tubes_color);
+  draw_rectangle(buffer, left_side-2, right_side+2, 0, top_part_hole+2, tubes_shadow_color);
+  x_coord first_third = left_side + width_tube/3;
+  draw_rectangle(buffer, left_side, first_third, 0, top_part_hole, tubes1_color);
+  x_coord second_third = first_third + width_tube/3;
+  draw_rectangle(buffer, first_third, second_third, 0, top_part_hole, tubes2_color);
+  draw_rectangle(buffer, second_third, right_side, 0, top_part_hole, tubes3_color);
+  draw_vertical_line(buffer, 0, top_part_hole, second_third+1, line_color);
+  draw_rectangle(buffer, left_side-4, right_side+4, top_part_hole-5, top_part_hole+2, tubes_shadow_color);
+  draw_rectangle(buffer, left_side-3, first_third-1, top_part_hole-4, top_part_hole+1, tubes1_color);
+  draw_rectangle(buffer, first_third-1, second_third+3, top_part_hole-4, top_part_hole+1, tubes2_color);
+  draw_vertical_line(buffer, top_part_hole-4, top_part_hole+1, second_third, line_color);
+  draw_rectangle(buffer, second_third+1, right_side+3, top_part_hole-4, top_part_hole+1, tubes3_color);
 
   // Tubo de abajo
-  draw_rectangle(buffer, left_side, right_side, bottom_part_hole, ground_height-1, tubes_color);
-}
+  draw_rectangle(buffer, left_side-2, right_side+2, bottom_part_hole-2, ground_height-1, tubes_shadow_color);
+  draw_rectangle(buffer, left_side, first_third, bottom_part_hole, ground_height-1, tubes1_color);
+  draw_rectangle(buffer, first_third, second_third, bottom_part_hole, ground_height-1, tubes2_color);
+  draw_rectangle(buffer, second_third, right_side, bottom_part_hole, ground_height-1, tubes3_color);
+  draw_vertical_line(buffer, bottom_part_hole, ground_height-1, second_third, line_color);
+  draw_rectangle(buffer, left_side-4, right_side+4, bottom_part_hole-5, bottom_part_hole+2, tubes_shadow_color);
+  draw_rectangle(buffer, left_side-3, first_third+1, bottom_part_hole-4, bottom_part_hole+1, tubes1_color);
+  draw_rectangle(buffer, first_third-1, second_third+3, bottom_part_hole-4, bottom_part_hole+1, tubes2_color);
+  draw_vertical_line(buffer, bottom_part_hole-4, bottom_part_hole+1, second_third+1, line_color);
+  draw_rectangle(buffer, second_third+2, right_side+3, bottom_part_hole-4, bottom_part_hole+1, tubes3_color);}
 
 /* Dibuja en buffer todos los tubos de game
  */
