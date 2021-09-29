fa729a7d89113f67fa086043a42b3583a6961bdd
Iván Renison <ivan.renison@mi.unc.edu.ar>
2021-09-29 14:13:40 -0300

    Actualizada distencia al primer tubo al comienzo

--- xv6-public/flappy_bird/flappy_bird_logic.c
+++ xv6-public/flappy_bird/flappy_bird_logic.c
@@ -31,7 +31,7 @@ init_game(int seed)
   game.vertical_speed = 0;
 
   global_seed = seed;
-  game.first_tube_x = 50;
+  game.first_tube_x = VGA_graphic_width/3;
 
   game.hole_tubes_y[0] = ground_height/2;
   for(uint j = 1u; j < amount_of_tubes; j++){
